e23734b22b95933b2d97b9111671edac3eb87856
janaestewart <janae.stewart94@gmail.com>
2022-03-21 23:03:20 -0400

    changed toast to resultTextView

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button =findViewById(R.id.button3)
+
+        rollButton.setOnClickListener{
+//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+//            val resultTextView: TextView = findViewById(R.id.textView)
+//            resultTextView.text = "6"
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return(1..numSides).random()
+    }
+}
